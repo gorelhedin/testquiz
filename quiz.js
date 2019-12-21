e992c0e42420685cbaf8c00617cd0d76cbb94643
Görel Hedin <gorel@cs.lth.se>
2019-12-21 21:14:16 +0100

    Factored out js code

---
+++ quiz.js
@@ -0,0 +1,59 @@
+(function(){
+
+
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+
+
+function buildQuiz(){
+  const output = [];
+
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+      const answers = [];
+      for(letter in currentQuestion.answers){
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+          );
+      }
+
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("<br>")} </div><br>`
+      );
+    }
+  );
+
+  quizContainer.innerHTML = output.join('');
+}
+
+function showResults(){
+  const answerContainers = quizContainer.querySelectorAll(".answers");
+  let numCorrect = 0;
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    if(userAnswer===currentQuestion.correctAnswer){
+      numCorrect++;
+
+      answerContainers[questionNumber].style.color = "lightgreen";
+    }
+    else {
+      answerContainers[questionNumber].style.color = "red";
+    }
+  });
+  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+buildQuiz();
+
+submitButton.addEventListener("click", showResults);
+
+})();
